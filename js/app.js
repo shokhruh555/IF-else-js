ae91f1e445c374306dc1fba04996407fadde11fb
Shokhruh555 <Shohruhismatov54@gmail.com>
2024-03-23 15:41:29 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,199 @@
+'use trict'
+// IF 12
+// let x = 1;
+
+// let y = 2;
+
+// let z = 3;
+
+// if (x <= y && x <= z) {
+//     console.log(x);
+// }else if (y <= x && y <= z) {
+//     console.log(y);
+// }else {
+//     console.log(z);
+// }
+
+
+// IF 13
+// let x = 1;
+
+// let y = 3;
+
+// let z = 9;
+
+// if (x >= y && x <= z) {
+//     console.log(x);
+// }else if (y >= x && y <= z) {
+//     console.log(y);
+// }else{
+//     console.log(z);
+// }   
+
+// IF 16
+// let x = 1;
+
+// let y = 4;
+
+// let z = 8;
+
+// if (x < y && x < z) {
+//     console.log(2*x);
+//     console.log(2*y);
+//     console.log(2*z);
+// }else{
+//     console.log(-x);
+//     console.log(-y);
+//     console.log(-z);
+// }
+
+// IF 15
+
+// let x = 5
+
+// let y = 7
+
+// let z = 8
+
+// if (z > x && y > x) {
+//     console.log(z+y);
+// }else if (x > y && x > y) {
+//     console.log(z+x);
+// }else if (x > z && y > z) {
+//     console.log(z+x);
+// }
+
+// IF 17
+
+// let x = 8;
+
+// let y = 7;
+
+// let z = 9;
+
+// if (x < y && x < z) {
+//     console.log(2*x);
+//     console.log(2*y);
+//     console.log(2*z);
+// }else if (x > y && x > z) {
+//     console.log(2*x);
+//     console.log(2*y);
+//     console.log(2*z);
+// }else{
+//     console.log(-x);
+//     console.log(-y);
+//     console.log(-z);
+// }
+
+
+// IF 18
+
+// let x = 10
+
+// let y = 10
+
+// let z = 7
+
+// if (x === y) {
+//    if( x > z){
+//     console.log(`z tartib raqami 1`);
+// } else{
+//     console.log(` tartib raqami 3`);
+// }
+// }else if (x === z) {
+//     console.log(4);
+// }else{
+//     console.log(7);
+// }
+
+// IF-19 
+ 
+// let x = 3
+// let y = 2
+// let z = 3
+// let e = 3
+ 
+// if(x ===  y, y === z){ 
+//     console.log(4); 
+// }else if(x === y , y === e){ 
+//     console.log(3); 
+// }else if(x === z , e === z){ 
+//     console.log(2); 
+// }else if(e === y , y === z){ 
+//     console.log(1); 
+// }else{ 
+//     console.log(); 
+// } 
+ 
+// IF-20 
+ 
+// let x = 5
+// let y = 11 
+// let z = 12
+ 
+// if(x < y && y < z){ 
+//     console.log("y" , y - x); 
+// }else if(x < z && y > z){ 
+//     console.log("z" , z - x); 
+// }else{ 
+//     console.log(); 
+// } 
+ 
+// IF-21 
+ 
+    // let aX = 10
+    // let aY = 4 
+    
+    // if(aX === 0 && aY === 0){ 
+    //     console.log(0); 
+    // }else if(aX !== 0 && aY === 0){ 
+    //     console.log(1 , 2); 
+    // }else if(aY !== 0 && aX === 0){ 
+    //     console.log(1 , 2); 
+    // }else{ 
+    //     console.log(3); 
+    // } 
+ 
+// IF-22 
+ 
+// let aX = 8 
+// let aY = -3 
+ 
+// if(aX > 0 && aY > 0){ 
+//     console.log(1); 
+// }else if(aX < 0 && aY > 0){ 
+//     console.log(2); 
+// }else if(aX < 0 && aY < 0){ 
+//     console.log(3); 
+// }else if(aX > 0 && aY < 0){ 
+//     console.log(4); 
+// }else{ 
+//     console.log(Error); 
+// } 
+ 
+// IF-23 
+ 
+// let aX1 = 3 
+// let aY1 = 5 
+// let aX2 = 3 
+// let aY2 = 5 
+// let aX3 = 8 
+// let aY3 = 5 
+// let aX4 = 8 
+// let aY4 = 3 
+ 
+// if(aX4 === aX3 && aY2 === aY3){ 
+//     console.log(aX2); 
+//     console.log(aY4); 
+// }else if(aX3 === aX4 && aY1 === aY4){ 
+//     console.log(aX1); 
+//     console.log(aY3); 
+// }else if(aX1 === aX2 && aY1 === aY4){ 
+//     console.log(aX4); 
+//     console.log(aY2); 
+// }else if(aX1 === aX2 && aY2 === aY3){ 
+//     console.log(aX3); 
+//     console.log(aY1); 
+// }else{ 
+//     console.log(Error); 
+// }
